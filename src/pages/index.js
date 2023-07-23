121c267d201e83635d6eccd79e7c96a0c0aaf0f0
Caleb Behayilu <CalebBehayilu@gmail.com>
2023-07-23 01:26:49 -0700

    first stable commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
-import { Inter } from "next/font/google";
+import { Roboto ,Montserrat } from "next/font/google";
 import Question from "./question";
 import data from "./data";
 import { useEffect } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ weight: ['400' , '300' , '100'] ,subsets: ["latin"] });
+const montserrat = Montserrat({
+  weight: ['400' , '700'],
+  subsets: ['latin'],
+  variable: '--font-montserrat'
 
+})
 export default function Home() {
   useEffect(() => {
   }, [data]);
@@ -12,7 +17,7 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${montserrat.className}`}
     >
       <Question questions={data} />
     </main>
